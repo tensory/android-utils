a6762c073ff4c25dc744a941908dc53e5a45d613
Ari Lacenski <ari@mangohealth.com>
2016-01-08 07:49:12 -0800

    Non-destructively rename files by copying instead of moving.

--- android_drawables/rename_drawables.py
+++ android_drawables/rename_drawables.py
@@ -2,6 +2,8 @@
 # Ari Lacenski 20151218
 
 import sys, os
+import shutil
+from string import rstrip
 from densities import for_all_densities
 
 def error_no_rename():
@@ -9,7 +11,7 @@ def error_no_rename():
 
 def error_usage():
 	print("Usage: " + __file__ + " [source_path] [new_name]")
-	print("Example: " + __file__ + " /Users/ari/Downloads/ic_launcher launcher_icon")
+	print("Example: " + __file__ + " /Users/ari/Downloads/ic_launcher/ launcher_icon")
 
 # source_dir should contain drawable- subdirectories with the image at different densities
 # drawable-mdpi, drawable-hdpi, drawable-xhdpi, and so on
@@ -33,12 +35,16 @@ if not new_name:
 
 # the unique filename of the asset: 
 # ic_launcher.png
-asset_filename = os.path.basename(source_dir) + ".png"
+asset_filename = os.path.basename(source_dir.rstrip(os.sep)) if source_dir.endswith(os.sep) else os.path.basename(source_dir)
+asset_filename = asset_filename + ".png"
 
 def move_file(density_prefix, source_dir, asset_filename, new_name):
-	drawable_dir = "drawable-" + density_prefix
 
 	# confirm that the file exists at the expected density
+	if not os.path.exists(source_dir):
+		print(source_dir + " does not exist.")
+
+	drawable_dir = "drawable-" + density_prefix
 	source_asset_path = os.path.join(source_dir, drawable_dir, asset_filename)
 	new_asset_path = os.path.join(source_dir, drawable_dir, new_name + ".png")
 
@@ -48,14 +54,30 @@ def move_file(density_prefix, source_dir, asset_filename, new_name):
 		print(source_asset_path + " does not exist. Skipping.")
 
 	else:
-		# move (destructively) the old path to the new path
-		os.rename(source_asset_path, new_asset_path)
+		# copy the old path to the new path
+		shutil.copy(source_asset_path, new_asset_path)
 
 		if not open(new_asset_path, "r"):
 			print("Moving " + source_asset_path + " failed.")
 			exit()
 		else:
+			# clean up the old file
+			os.remove(source_asset_path)
 			print(new_asset_path + " created.")
+		
+	
+# prune trailing slashes to prepare dirname
+source_dir = source_dir.rstrip(os.sep)
 
+# os.path.abspath(os.path.join(source_dir, os.path.pardir)) if source_dir.endswith(os.sep) else 
+parent_name = os.path.dirname(source_dir)
+new_asset_dir = os.path.join(parent_name, new_name)
+shutil.copytree(source_dir, new_asset_dir)
 
-for_all_densities(move_file, source_dir=source_dir, asset_filename=asset_filename, new_name=new_name)
+for_all_densities(move_file, source_dir=new_asset_dir, asset_filename=asset_filename, new_name=new_name)
+
+if not os.path.exists(new_asset_dir):
+	print("Could not rename directory to %s." % new_name)
+else:
+	shutil.rmtree(source_dir)
+	print("Done renaming %s to %s." % (os.path.basename(source_dir), new_name))
